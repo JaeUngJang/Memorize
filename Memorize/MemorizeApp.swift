5f39592f45110424c57bc4bf2f6c6e34e119949f
Ung <jjw3435@naver.com>
2023-05-02 20:20:09 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by JaeUngJang on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
