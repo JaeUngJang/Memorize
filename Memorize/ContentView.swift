83a2532fd9a04e9d6609e86697c4ad85bbbab0e2
Jae Ung Jang <jjw3435@naver.com>
2023-05-12 00:40:24 +0900

    after lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,35 +8,120 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var carEmojis: [String] = ["🚂", "✈️", "🚲", "🚁", "🏎", "🚌", "🚍", "🚎", "🚐", "🚑", "🚒", "🚓", "🚔", "🚕", "🚖", "🚗", "🚙", "🚚", "🛻", "🛴", "🛵", "🏍", "🚜","🚞"]
+    var foodEmojis: [String] = ["🌭", "🌮", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🫐", "🍈", "🍒", "🍑", "🥭", "🍍", "🥥", "🥝", "🍅", "🍆", "🌽", "🥕", "🫒", "🧅", "🥔"]
+    var faceEmojis: [String] = ["😀", "😃", "😄", "😁", "😆", "🥹", "😅", "😂", "🤣", "🥲", "☺️", "😊", "😇", "🙂", "🙃", "😉", "😌", "😍", "🥰", "😘", "😗", "😙", "😚", "😋", "😝"]
+    
+    @State var currentEmojis: [String] = []
+    @State var emojiCount: Int = 20
+    
+    
     var body: some View {
 
         
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            Text("Memorize!")
+                .font(.largeTitle)
+                
+            
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+//                    ForEach(carEmojis[0..<emojiCount], id: \.self) { emoji in
+                    ForEach(currentEmojis.shuffled(), id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+
+            Spacer()
+            
+            HStack {
+                Spacer()
+                vehicle
+                Spacer()
+                food
+                Spacer()
+                face
+                Spacer()
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
-        
-        
-        
+    }
+    
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < carEmojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+    
+    var vehicle: some View {
+        Button {
+            currentEmojis = carEmojis
+        } label: {
+            VStack {
+                Image(systemName: "car")
+                Text("Vehicle")
+                    .font(.caption)
+            }
+        }
+    }
+    
+    var food: some View {
+        Button {
+            currentEmojis = foodEmojis
+        } label: {
+            VStack {
+                Image(systemName: "fork.knife")
+                Text("Food")
+                    .font(.caption)
+            }
+        }
+    }
+    var face: some View {
+        Button {
+            currentEmojis = faceEmojis
+        } label: {
+            VStack {
+                Image(systemName: "face.smiling")
+                Text("Face")
+                    .font(.caption)
+            }
+        }
     }
 }
 
 struct CardView: View {
     
+    var content: String
     @State var isFaceUp: Bool = true
     
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
-            
             if isFaceUp {
                 shape.fill(.white)
-                shape.stroke(lineWidth: 3)
-                Text("✈️")
+                shape.strokeBorder(lineWidth: 3)
+                Text(content)
                     .font(.largeTitle)
             } else {
                 shape.fill()
